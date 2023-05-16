8ea83ee2798bf0ad1953ca7d425b0d6705027fec
Nash-equilbrm <dohoainam17052001@gmail.com>
2023-05-16 12:38:53 +0700

    + Play UI

---
+++ Assets/Scripts/GamePlay/Cards/Card.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:90a39e4cca97f4795048097b0d6c0a2050b1b74086e1bdb3b95269c8a85421c6
+size 207
---
+++ Assets/Scripts/MeshSimplifier.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:49672dcd67047addd3cb955fe703e8d48ca982e41f8c081090aa70d41c04944b
+size 1445
