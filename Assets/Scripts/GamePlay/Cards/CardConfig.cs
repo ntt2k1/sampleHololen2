c614b0281006cedd9e1737e104b1bf51d974e0d2
Nash-equilbrm <dohoainam17052001@gmail.com>
2023-05-18 10:22:53 +0700

    +display skeleton models by hand pointer raycast to evironment mesh

--- Assets/Scripts/GamePlay/Cards/CardConfig.cs
+++ Assets/Scripts/GamePlay/Cards/CardConfig.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:045e82b9c392dcad2d0da0906ddad1f07536b4fa802e73f9afe7b2bf1afa2005
-size 312
+oid sha256:31bd8fc1fc84f8bdb58e2e01e80bef2755d35ac27796f2b9b92233925650e020
+size 276
---
+++ Assets/Scripts/GamePlay/Player/Player.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:71962864f749c4f9ee54a3d425dc39bd44567426d8db91a9bc724d78fcf6bc5d
+size 5325
